0d82a0f99780656bbd0cabbcc8dbb58baf8e0991
Hyunkuk <boris0815@naver.com>
2023-04-10 18:30:34 +0900

    [feat] : update domain module

---
+++ domain/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("java-library")
+    alias(libs.plugins.org.jetbrains.kotlin.jvm)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+    implementation(libs.coroutine)
+}
