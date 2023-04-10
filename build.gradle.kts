7d0dc684e47e69fdf3353d9f2addf0e928edb2e6
Hyunkuk <boris0815@naver.com>
2023-04-10 18:31:14 +0900

    [chore] : project setting

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    alias(libs.plugins.com.android.application) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.com.android.library) apply false
+    alias(libs.plugins.hilt.plugin) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "CoroutineStudy"
+include(":app")
+include(":presentation")
+include(":domain")
+include(":data")
