abd2dd21c2379d6acf481fa3dbb8904ec0d82feb
Hyunkuk <boris0815@naver.com>
2023-04-10 15:40:37 +0900

    [chore] : app base

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My Application"
+include(":app")
+include(":presentation")
+include(":domain")
+include(":data")
