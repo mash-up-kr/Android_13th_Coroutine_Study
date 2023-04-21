86ed403544468117ba4d8746d3b32bc28cf5424c
ze-zeh <chojh922@gmail.com>
2023-04-21 19:32:08 +0900

    [style] color 수정

--- presentation/src/main/java/com/example/presentation/ui/theme/Color.kt
+++ presentation/src/main/java/com/example/presentation/ui/theme/Color.kt
@@ -8,7 +8,7 @@ val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
 val BasePurple = Color(0xFF992DDC)
-val BasePink = Color(0x78EB26B4)
+val BasePink = Color(0xFFF699DD)
 
 val DividerGray = Color(0xFFD9D9D9)
 val GitHubLink = Color(0xFF2141B4)
