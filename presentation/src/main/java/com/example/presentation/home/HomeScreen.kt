88dd46361b1316cb8c09d277fb94a6e5925e7a93
정민지 <s2018s38@e-mirim.hs.kr>
2023-04-25 23:53:10 +0900

    [feat] 사용자 이름을 검색할 수 있도록 TextField 추가 및 홈 UI 개선

--- presentation/src/main/java/com/example/presentation/home/HomeScreen.kt
+++ presentation/src/main/java/com/example/presentation/home/HomeScreen.kt
@@ -1,32 +1,20 @@
 package com.example.presentation.home
 
-import androidx.annotation.StringRes
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.paddingFromBaseline
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
-import com.bumptech.glide.integration.compose.GlideImage
-import com.example.presentation.R
+import androidx.compose.ui.unit.sp
+import com.example.presentation.home.crew.StudyCrewList
 import com.example.presentation.model.MashUpCrew
-import com.example.presentation.ui.theme.MashUpCoroutineStudyTheme
 
 /**
  * CoroutineStudy
@@ -34,74 +22,37 @@ import com.example.presentation.ui.theme.MashUpCoroutineStudyTheme
  * @created 2023/04/10
  */
 
-/**
- * @author 재성
- * XML로 하실 분은 띠용하지 마시고 가볍게 삭제하시길
- * Compose로 하실 분들은 띠용하시고 이어서 작성하셔도 됩니당
- */
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
+    val textState = remember { mutableStateOf("") }
     val studyCrewList = MashUpCrew.values().toList()
-    Column(modifier = modifier.padding(8.dp)) {
-        StudyCrewList(studyCrewList = studyCrewList, modifier = Modifier)
 
-    }
-}
-
-@Composable
-fun StudyCrewList(
-    studyCrewList: List<MashUpCrew>,
-    modifier: Modifier = Modifier
-) {
-    LazyRow(modifier = modifier) {
-        items(studyCrewList) {
-            StudyCrew(userName = it.userName, imageUrl = it.profileImage, modifier = modifier)
-        }
-    }
-}
-
-@OptIn(ExperimentalGlideComposeApi::class)
-@Composable
-fun StudyCrew(
-    @StringRes userName: Int,
-    @StringRes imageUrl: Int,
-    modifier: Modifier = Modifier
-) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.padding(8.dp)
-    ) {
-        GlideImage(
-            model = stringResource(id = imageUrl),
-            contentDescription = stringResource(id = userName),
-            contentScale = ContentScale.Fit,
+    Column(modifier = modifier.padding(vertical = 8.dp)) {
+        // 검색어 입력창
+        TitleText(text = "Github 사용자를 검색해보세요")
+        TextField(
+            value = textState.value,
+            onValueChange = { changedText -> textState.value = changedText },
+            label = { Text("사용자 이름") },
             modifier = Modifier
-                .size(96.dp)
-                .clip(CircleShape)
-                .border(
-                    width = 1.dp,
-                    brush = Brush.verticalGradient(
-                        colors = listOf(Color.Blue, Color.Gray)
-                    ),
-                    shape = CircleShape
-                )
+                .padding(8.dp)
+                .fillMaxWidth()
         )
 
-        Text(
-            text = stringResource(id = userName),
-            style = MaterialTheme.typography.body2,
-            maxLines = 1,
-            modifier = Modifier.paddingFromBaseline(top = 24.dp)
-        )
+        // 즐겨찾기 (검색어 입력이 없을 때)
+        if (textState.value.isEmpty()) {
+            TitleText(text = "즐겨찾기 ✨")
+            StudyCrewList(studyCrewList = studyCrewList, modifier = Modifier)
+        }
     }
 }
 
-@Preview
 @Composable
-fun StudyCrewPreview() {
-    MashUpCoroutineStudyTheme {
-        Surface {
-            StudyCrew(R.string.jaesung, R.string.jaesung_url)
-        }
-    }
+fun TitleText(text: String) {
+    Text(
+        text = text,
+        modifier = Modifier.padding(8.dp),
+        style = TextStyle(fontWeight = FontWeight.Bold),
+        fontSize = 18.sp
+    )
 }
