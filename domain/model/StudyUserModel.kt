41ae9e59fb413eee5f5ff7c9ef191b97d83e8352
Kim Hyun Su <86879099+KimHance@users.noreply.github.com>
2023-04-25 23:42:37 +0900

    feat: searchModel생성

--- domain/model/StudyUserModel.kt
+++ domain/model/StudyUserModel.kt
@@ -1,13 +1,19 @@
 package model
 
-data class StudyUserModel(
-    val id: Int
-    val follwers: List<Follwer> = emptyList()
+data class SearchModel(
+    val searchList: List<User> = emptyList()
 ) {
-    data class Follwer(
-        val name: String = "",
+    data class User(
+        val name: String = ""
         val avatarUrl: String = "",
-        val userPageUrl: String = ""
-    )
+        val follwerCount: Int = "",
+        val follwers: List<Follwer> = emptyList()
+    ) {
+        data class Follwer(
+            val name: String = "",
+            val avatarUrl: String = "",
+            val userPageUrl: String = ""
+        )
+    }
 }
 
