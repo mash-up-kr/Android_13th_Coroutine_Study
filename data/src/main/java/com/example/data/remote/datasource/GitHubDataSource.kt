0752e1c0db12ef96daaf5fa37e04b8b4a4737dc4
Kim Hyun Su <86879099+KimHance@users.noreply.github.com>
2023-04-25 23:59:57 +0900

    feat: 데이터소스 작성

--- data/src/main/java/com/example/data/remote/datasource/GitHubDataSource.kt
+++ data/src/main/java/com/example/data/remote/datasource/GitHubDataSource.kt
@@ -1,6 +1,8 @@
 package com.example.data.remote.datasource
 
 import com.example.data.remote.response.follower.FollowerResponse
+import com.example.data.remote.response.search.SearchUserResponse
+import com.example.data.remote.response.user.UserResponse
 import com.example.data.remote.service.GitHubApiService
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -10,13 +12,33 @@ class GitHubDataSource @Inject constructor(
     private val service: GitHubApiService
 ) {
 
-    suspend fun getFollowers(userName: String): Flow<List<FollowerResponse>> = flow {
+    fun searchUsers(query: String): Flow<SearchUserResponse> = flow {
+        runCatching {
+            service.searchUsers(query)
+        }.onSuccess { response ->
+            emit(response)
+        }.onFailure { e ->
+            throw e
+        }
+    }
+
+    fun getFollowers(userName: String): Flow<List<FollowerResponse>> = flow {
         runCatching {
             service.getFollowers(userName)
         }.onSuccess { response ->
             emit(response)
-        }.onFailure {
-            emit(emptyList())
+        }.onFailure { e ->
+            throw e
+        }
+    }
+
+    fun getUser(userName: String): Flow<UserResponse> = flow {
+        runCatching {
+            service.getUser(userName)
+        }.onSuccess { response ->
+            emit(response)
+        }.onFailure { e ->
+            throw e
         }
     }
 }
