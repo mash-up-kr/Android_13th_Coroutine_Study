a5a21b518669f3806f73a0bf948d1ac3bf9441f6
JaesungLeee <viki0114@naver.com>
2023-04-29 00:28:27 +0900

    [Style] 공통 컴포저블 분리

--- presentation/src/main/java/com/example/presentation/home/StudyCrew.kt
+++ presentation/src/main/java/com/example/presentation/home/StudyCrew.kt
@@ -1,30 +1,22 @@
 package com.example.presentation.home
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
-import com.bumptech.glide.integration.compose.GlideImage
 import com.example.presentation.R
+import com.example.presentation.common.CircularImage
 import com.example.presentation.model.MashUpCrew
 import com.example.presentation.ui.theme.MashUpCoroutineStudyTheme
 
@@ -33,26 +25,6 @@ import com.example.presentation.ui.theme.MashUpCoroutineStudyTheme
  * @author jaesung
  * @created 2023/04/25
  */
-
-@Composable
-@OptIn(ExperimentalGlideComposeApi::class)
-fun CircularImage(imageUrl: Int, userName: Int, size: Int = 96) {
-    GlideImage(
-        model = stringResource(id = imageUrl),
-        contentDescription = stringResource(id = userName),
-        contentScale = ContentScale.Fit,
-        modifier = Modifier
-            .size(size.dp)
-            .clip(CircleShape)
-            .border(
-                width = 1.dp,
-                brush = Brush.verticalGradient(
-                    colors = listOf(Color.Blue, Color.Magenta)
-                ),
-                shape = CircleShape
-            )
-    )
-}
 
 @Composable
 fun StudyCrew(
@@ -75,7 +47,7 @@ fun StudyCrew(
 }
 
 @Composable
-fun StudyCrewList(
+fun UserList(
     studyCrewList: List<MashUpCrew>,
     modifier: Modifier = Modifier
 ) {
