c0a7349b2f7329d69b00953a313ed1f9b00de3ce
ze-zeh <chojh922@gmail.com>
2023-04-29 00:01:12 +0900

    [feat] 깃헙 토큰 헤더 추가

--- data/src/main/java/com/example/data/remote/RepoService.kt
+++ data/src/main/java/com/example/data/remote/RepoService.kt
@@ -1,12 +1,15 @@
 package com.example.data.remote
 
+import com.example.data.BuildConfig
 import com.example.data.remote.response.repo.GitHubRepoResponse
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Path
 
 interface RepoService {
     @GET("/users/{username}/repos")
     suspend fun getGitHubRepo(
+        @Header("Authorization") token: String = "token ${BuildConfig.GITHUB_TOKEN}",
         @Path("username") userName: String,
     ): List<GitHubRepoResponse>
 }
--- data/src/main/java/com/example/data/remote/SearchService.kt
+++ data/src/main/java/com/example/data/remote/SearchService.kt
@@ -1,12 +1,16 @@
 package com.example.data.remote
 
+import com.example.data.BuildConfig
 import com.example.data.remote.response.search.SearchResponse
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface SearchService {
-    @GET("/search/users?q=")
-    suspend fun getSearch(
-        @Path("search") search: String,
-    ): List<SearchResponse>
+    @GET("/search/users")
+    suspend fun searchUsers(
+        @Header("Authorization") token: String = "token ${BuildConfig.GITHUB_TOKEN}",
+        @Query("q") userName: String,
+    ): SearchResponse
 }
