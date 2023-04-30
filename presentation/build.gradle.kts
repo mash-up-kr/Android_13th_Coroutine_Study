dfae9014af3fe0194ff0e6f13c36b6cf93eecafc
Hyunkuk <boris0815@naver.com>
2023-05-01 00:06:56 +0900

    [chore] : add navigation and gson

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -6,19 +6,19 @@ plugins {
 }
 
 android {
-    namespace =  "com.example.presentation"
-    compileSdk =  33
+    namespace = "com.example.presentation"
+    compileSdk = 33
 
     defaultConfig {
-        minSdk  = 24
-        targetSdk =  33
+        minSdk = 24
+        targetSdk = 33
 
-        testInstrumentationRunner =  "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled  = false
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
@@ -26,8 +26,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility  =   JavaVersion.VERSION_1_8
-        targetCompatibility  =  JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
         jvmTarget = "1.8"
@@ -54,8 +54,8 @@ dependencies {
     implementation(libs.ui.tooling.preview)
     debugImplementation(libs.ui.tooling)
     implementation(libs.foundation)
-    implementation(libs.glide.compose)  // Compose 용
-    implementation(libs.glide.view)  // View System 용
+    implementation(libs.glide.compose) // Compose 용
+    implementation(libs.glide.view) // View System 용
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso)
@@ -64,4 +64,7 @@ dependencies {
     implementation(libs.hilt.android)
     kapt(libs.hilt.compiler)
     kapt(libs.glide.compiler)
+    implementation(libs.navigation.compose)
+    implementation(libs.gson)
+    implementation(libs.hilt.navigation.compose)
 }
