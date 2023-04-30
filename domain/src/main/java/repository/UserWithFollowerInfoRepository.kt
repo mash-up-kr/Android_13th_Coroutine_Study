ebf05645532648421d4e34a854dbc158b4adfdfd
ddyeon <cab05102@naver.com>
2023-05-01 00:28:59 +0900

    [feat] 검색결과 useacase, repository 추가

---
+++ domain/src/main/java/repository/UserWithFollowerInfoRepository.kt
@@ -0,0 +1,8 @@
+package repository
+
+import kotlinx.coroutines.flow.Flow
+import model.UserInfoResponse
+
+interface UserWithFollowerInfoRepository {
+    suspend fun getUserWithFollowerInfo(query: String): Flow<List<UserInfoResponse>>?
+}
---
+++ domain/src/main/java/usecase/GetUserWithFollowerInfoUseCase.kt
@@ -0,0 +1,14 @@
+package usecase
+
+import kotlinx.coroutines.flow.Flow
+import model.UserInfoResponse
+import repository.UserWithFollowerInfoRepository
+import javax.inject.Inject
+
+class GetUserWithFollowerInfoUseCase @Inject constructor(
+    private val userWithFollowerInfoRepository: UserWithFollowerInfoRepository
+) {
+    suspend operator fun invoke(query: String): Flow<List<UserInfoResponse>>? {
+        return userWithFollowerInfoRepository.getUserWithFollowerInfo(query)
+    }
+}
