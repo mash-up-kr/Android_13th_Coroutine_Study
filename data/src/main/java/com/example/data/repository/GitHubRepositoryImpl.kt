5b2f8c048de190f8278431ee58af2f4f5aee349b
Hyunkuk <boris0815@naver.com>
2023-05-01 00:17:23 +0900

    [refactor] : reformat

--- data/src/main/java/com/example/data/repository/GitHubRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/GitHubRepositoryImpl.kt
@@ -4,7 +4,6 @@ import com.example.data.model.toModel
 import com.example.data.source.remote.datasource.FollowerDataSource
 import com.example.data.source.remote.datasource.SearchDataSource
 import com.example.data.source.remote.datasource.UserDataSource
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.*
 import model.User
@@ -25,37 +24,44 @@ class GitHubRepositoryImpl @Inject constructor(
     @OptIn(FlowPreview::class)
     override fun getUserInfoList(query: String, page: Int): Flow<UserListResponse> {
         return flow {
-            searchDataSource.getUserInfoList(query = query, page = page).collect { userListResponse ->
-                if (userListResponse != null) {
-                    val flow = userListResponse.items.asFlow().flatMapMerge {
-                        flow {
-                            if (it.login == null) {
-                                return@flow
+            searchDataSource.getUserInfoList(query = query, page = page)
+                .collect { userListResponse ->
+                    if (userListResponse != null) {
+                        val flow = userListResponse.items.asFlow().flatMapMerge {
+                            flow {
+                                if (it.login == null) {
+                                    return@flow
+                                }
+                                val userFlow = userDataSource.getUser(userName = it.login)
+                                val followerFlow =
+                                    followerDataSource.getFollowers(userName = it.login)
+                                combine(userFlow, followerFlow) { userResponse, followerResponses ->
+                                    User(
+                                        login = userResponse?.login,
+                                        blog = userResponse?.blog,
+                                        followers = userResponse?.followers,
+                                        public_repos = userResponse?.publicRepos,
+                                        url = userResponse?.htmlUrl,
+                                        avatarUrl = userResponse?.avatarUrl,
+                                        followersList = followerResponses.map { followerResponse ->
+                                            followerResponse.toModel()
+                                        },
+                                    )
+                                }.collect { user ->
+                                    emit(user)
+                                }
                             }
-                            val userFlow = userDataSource.getUser(userName = it.login)
-                            val followerFlow = followerDataSource.getFollowers(userName = it.login)
-                            combine(userFlow, followerFlow) { userResponse, followerResponses ->
-                                User(
-                                    login = userResponse?.login,
-                                    blog = userResponse?.blog,
-                                    followers = userResponse?.followers,
-                                    public_repos = userResponse?.publicRepos,
-                                    url = userResponse?.htmlUrl,
-                                    avatarUrl = userResponse?.avatarUrl,
-                                    followersList = followerResponses.map { followerResponse ->
-                                        followerResponse.toModel()
-                                    },
-                                )
-                            }.collect { user ->
-                                emit(user)
-                            }
-                        }
-                    }.toList()
-                    emit(UserListResponse(items = flow, total_count = userListResponse.total_count))
-                } else {
-                    emit(UserListResponse(emptyList(), 0))
+                        }.toList()
+                        emit(
+                            UserListResponse(
+                                items = flow,
+                                total_count = userListResponse.total_count,
+                            ),
+                        )
+                    } else {
+                        emit(UserListResponse(emptyList(), 0))
+                    }
                 }
-            }
-        }.flowOn(Dispatchers.IO)
+        }
     }
 }
